6fb31f475cb127674b8f004910837c2501c3ecb2
edalx <edalx32@gmail.com>
2023-07-20 00:53:13 -0300

    clx2 project test

---
+++ calxApp/src/screens/CalculadoraScreen.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import { styles } from '../theme/appTheme'
+
+export const CalculadoraScreen = () => {
+  return (
+    <View style={styles.calculadoraContainer}>
+        <Text style={styles.resultadoPequeño}>1,500.00</Text>
+        <Text style={styles.resultado}>1,500.00</Text>
+        <View>
+            <View style={styles.boton}>
+                <Text style={styles.botonTexto}>1</Text>
+            </View>
+        </View>
+    </View>
+
+  )
+}
