6447dddfa38258d630a52b5a1d81db61fd17cecc
edalx <edalx32@gmail.com>
2023-09-23 19:31:10 -0300

    HooksApi pokemon

--- Pokedex/src/screens/HomeScreen.tsx
+++ Pokedex/src/screens/HomeScreen.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { Image, Text, View } from 'react-native'
 import { styles } from '../theme/appTheme';
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { usePokemonPaginated } from '../hooks/usePokemonPaginated';
 
 export const HomeScreen = () => {
   const { top } = useSafeAreaInsets();
+  const { simplePokemonList } = usePokemonPaginated();
   return (
     <>
       <Image
@@ -14,7 +16,7 @@ export const HomeScreen = () => {
       <Text style={{
         ...styles.title,
         ...styles.globalMargin,
-        color:'black',
+        color: 'black',
         top: top + 20,
       }}>PokemonScreen</Text>
     </>
